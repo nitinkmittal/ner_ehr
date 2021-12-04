37905f950db84124e98b23ac080701b5214b64fe
nitinkmittal <nitinmittal243@gmail.com>
2021-12-04 17:16:47 -0500

    Added more constant variables

--- src/ner_ehr/data/__init__.py
+++ src/ner_ehr/data/__init__.py
@@ -5,3 +5,6 @@ class Constants(Enum):
     UNTAG_ENTITY_LABEL = "O"  # outside entity
     PAD_TOKEN = "<PAD>"
     UNK_TOKEN = "<UNK>"
+    PAD_TOKEN_IDX = 0
+    UNK_TOKEN_IDX = 1
+    UNTAG_ENTITY_INT_LABEL = 0
