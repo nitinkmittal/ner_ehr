36bab5eea6a51f497ceaa0d26af7c1864c616618
nitinkmittal <nitinmittal243@gmail.com>
2021-12-07 15:50:28 -0500

    Removed extra code

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,11 +1,11 @@
-from typing import Union, List
-from pathlib import Path
-from ner_ehr.data.variables import AnnotationTuple, LongAnnotationTuple
-from ner_ehr.data.utils import df_to_namedtuples, TokenEntityVocab
-from ner_ehr.data.ehr import EHR
 import os
 from glob import glob
-from sklearn.preprocessing import LabelEncoder
+from pathlib import Path
+from typing import List, Union
+
+from ner_ehr.data.ehr import EHR
+from ner_ehr.data.utils import df_to_namedtuples
+from ner_ehr.data.variables import AnnotationTuple
 
 
 def read_annotatedtuples(dir: Union[str, Path]) -> List[AnnotationTuple]:
@@ -39,39 +39,3 @@ def read_annotatedtuples(dir: Union[str, Path]) -> List[AnnotationTuple]:
         )
 
     return annotatedtuples
-
-
-def build_vocab(annotatedtuples: List[AnnotationTuple]) -> TokenEntityVocab:
-    """Build vocab from list of annotated tuples.
-
-    Args:
-        annotatedtuples: list of AnnotatedToken tuples
-                [
-                    Annotation(
-                        doc_id='100035',
-                        token='Admission',
-                        start_idx=0,
-                        end_idx=9,
-                        entity='O'),
-                    Annotation(
-                        doc_id='100035',
-                        token='Date',
-                        start_idx=10,
-                        end_idx=14,
-                        entity='O'),
-                ]
-
-    Returns:
-        TokenEntityVocab object
-    """
-    vocab = TokenEntityVocab()
-    vocab.fit(annotatedtuples=annotatedtuples)
-    return vocab
-
-
-def label_encoder(
-    annotatedtuples: Union[List[AnnotationTuple], List[LongAnnotationTuple]]
-) -> LabelEncoder:
-    encoder = LabelEncoder()
-    encoder.fit([annotatedtuple.entity for annotatedtuple in annotatedtuples])
-    return encoder
