bd640d47f60efd89e293233a953cdaa3e4b364f8
Arushi <aarushie2004@gmail.com>
2021-12-06 22:49:20 -0500

    updated baseline model to utilize common util functions

--- scripts/baseline.py
+++ scripts/baseline.py
@@ -1,63 +1,53 @@
 import glob
+import os
+from os import listdir
+from os.path import isfile, join
 from typing import List
-import pandas as pd
+from tqdm import tqdm
 
+import pandas as pd
+import numpy as np
 from sklearn.metrics import classification_report
 
-from datasets.dataset import NerDataset
-from datasets.vocab import Vocab
+from ner_ehr.data import Constants
+from ner_ehr.data.variables import AnnotationTuple
+from ner_ehr.data.vocab import TokenEntityVocab
+from utils import read_annotatedtuples
 
 
-class BaseModel:
-    def __init__(self, root_dir: str):
-        self.root_dir = root_dir
-
-    def train(self, dl: List):
-        raise NotImplementedError("Not implemented..")
-
-    def fit(self, dataset: List[List], model) -> (List, List):
-        raise NotImplementedError("Not implemented..")
-
-    def evaluate(self, y_gold: List, y_pred: List) -> dict:
-        score = classification_report(y_gold, y_pred)
-        return score
-
-
-class Baseline(BaseModel):
+class Baseline:
     """Creates a baseline model of dictionary and returns the
     performance metrics on validation and test dataset"""
 
-    def __init__(self):
-        super().__init__(root_dir)
+    def __init__(self, vocab: TokenEntityVocab):
+      
+        self.vocab = vocab
 
-    def train(self, dl: List):
-        raise NotImplementedError("Not implemented..")
-
-    def fit(self, dataset: List[List], vocab: dict) -> (List, List):
+    def fit(self, dataset: List[AnnotationTuple]) -> (List, List):
         """
         Evaluates if the token from the given dataset is present in vocab and
         returns the gold label and predicted labels of the given dataset.
 
         Args:
-            dataset: data in the form of list of list
-            vocab: dictionary containing voab from training dataset
+            dataset: dataset in the form of list of annotation tuples
         Returns:
-            y_gold: true labels of the dataset
-            y_pred: predicted labels of the dataset
+            y_gold: list of true labels
+            y_pred: list of predicted labels
         """
 
         y_gold = []
         y_pred = []
 
         # check if token exists in the vocab, else assign it to 'O' tag
-        for row in dataset:
-            token = row[0]
-            if token not in vocab:
-                y_pred.append("O")
+        for row in tqdm(dataset, leave=False, position=0):
+            token = self.vocab._to_lower(row.token)
+            pred_entity = self.vocab.token_entity_freq[token]
+            if len(pred_entity) == 0:
+                y_pred.append(Constants.UNTAG_ENTITY_LABEL.value)
             else:
-                y_pred.append(max(vocab[token], key=vocab[token].get))
+                y_pred.append(sorted(pred_entity.items(), key=lambda x: x[1], reverse=True)[0][0])
 
-            y_gold.append(row[-1])
+            y_gold.append(row.entity)
 
         return y_gold, y_pred
 
@@ -73,36 +63,35 @@ class Baseline(BaseModel):
             for all entity classes
         """
 
-        labels = set(gold_labels)
-        labels.remove("O")
-
-        sorted_labels = sorted(labels, key=lambda name: (name[1:], name[0]))
-        score = classification_report(
-            gold_labels, pred_labels, labels=sorted_labels
-        )
-        # score = classification_report(gold_labels, pred_labels)
+        score = classification_report(gold_labels, pred_labels, labels=np.unique(gold_labels))
         return score
 
 
 if __name__ == "__main__":
 
-    root_dir = "../notebooks/out"
-    baseline_model = Baseline()
+    root_dir = "../processed"
 
-    train = NerDataset(f"{root_dir}/train/")
-    val = NerDataset(f"{root_dir}/val/", expand_vocab=False)
-    test = NerDataset(f"{root_dir}/test/", expand_vocab=False)
+    # Reading files and creating vocab
+    train_annotatedtuples = read_annotatedtuples(
+        dir=os.path.join(root_dir, "train")
+    )
+    vocab = TokenEntityVocab(to_lower=True)
+    vocab.fit(annotatedtuples=train_annotatedtuples)
 
-    datasets = {"train": train, "val": val, "test": test}
+    val_annotatedtuples = read_annotatedtuples(
+        dir=os.path.join(root_dir, "val")
+    )
+    test_annotatedtuples = read_annotatedtuples(
+        dir=os.path.join(root_dir, "test")
+    )
 
-    train_vocab = datasets["train"].vocab.stats
-    val_ds = datasets["val"]
-    test_ds = datasets["test"]
+    baseline = Baseline(vocab=vocab)
 
-    val_gold, val_pred = baseline_model.fit(val_ds, train_vocab)
-    score_valid = baseline_model.evaluate(val_gold, val_pred)
+    # Performance on val and test data
+    val_gold, val_pred = baseline.fit(val_annotatedtuples)
+    score_valid = baseline.evaluate(val_gold, val_pred)
     print(f"Validation metrics: {score_valid}")
 
-    test_gold, test_pred = baseline_model.fit(test_ds, train_vocab)
-    score_test = baseline_model.evaluate(test_gold, test_pred)
+    test_gold, test_pred = baseline.fit(test_annotatedtuples)
+    score_test = baseline.evaluate(test_gold, test_pred)
     print(f"Test metrics: {score_test}")
