31c34b8a5db71444d44943b4542072b7823d3b4b
nitinkmittal <nitinmittal243@gmail.com>
2021-12-07 21:06:35 -0500

    Updated constants for PAD and outside entity

--- src/ner_ehr/data/__init__.py
+++ src/ner_ehr/data/__init__.py
@@ -2,9 +2,11 @@ from enum import Enum
 
 
 class Constants(Enum):
-    UNTAG_ENTITY_LABEL = "O"  # outside entity
     PAD_TOKEN = "<PAD>"
+    PAD_TOKEN_ENTITY_LABEL = "P"  # garbage entity
+    PAD_TOKEN_ENTITY_INT_LABEL = 0  # garbage entity label
     UNK_TOKEN = "<UNK>"
     PAD_TOKEN_IDX = 0
     UNK_TOKEN_IDX = 1
-    UNTAG_ENTITY_INT_LABEL = 0
+    UNTAG_ENTITY_LABEL = "O"  # outside entity
+    UNTAG_ENTITY_INT_LABEL = 1  # outside entity label
